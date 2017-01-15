07501faed87b2d9a85bfc4f09d4d249c3bc8f46c
Ibrahim Shaikh <ibrahims123457@yahoo.com>
2017-01-15 13:03:32 -0800

    Starting a command line weather application using node

---
+++ weather.js
@@ -0,0 +1,8 @@
+/**
+ * Created by IbrahimShaikh on 1/15/17.
+ *
+ * Command line weather application that takes in a US zip code as its input and returns the weather for that zip code.
+ */
+
+var latitude = "";
+var longitude = "";
