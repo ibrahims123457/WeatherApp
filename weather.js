22f742004dac48f794cddd52566adef05cabf8dd
Ibrahim Shaikh <ibrahims123457@yahoo.com>
2017-01-15 21:39:03 -0800

    Completed the application. Used the API that I made for Dark Sky and added necessary error handling code.

--- weather.js
+++ weather.js
@@ -6,14 +6,17 @@
 
 var https = require("https");
 var http = require("http");
+var darkSky = require("./DarkSkyAPI");
 
 var latitude = "";
 var longitude = "";
-var zip = "95757";
+var zip = process.argv[2];
+var cityName = "";
+var stateName = "";
 
 
 //Use the Google Maps API to get the longitude and latitude of the zip code
-var res = http.get("http://maps.googleapis.com/maps/api/geocode/json?address=" + zip, function (req) {
+var mapRes = http.get("http://maps.googleapis.com/maps/api/geocode/json?address=" + zip, function (req) {
 
     //variable holding the body of the req
     var body = "";
@@ -25,14 +28,30 @@ var res = http.get("http://maps.googleapis.com/maps/api/geocode/json?address=" +
     });
 
     //req end event
-    req.on('end', function (chunk) {
-        //parse the information and assign it to the appropriate variables
-        var data = JSON.parse(body);
+    req.on('end', function () {
+        //parse the information
+        try {
+            var data = JSON.parse(body);
 
-        latitude = data.results[0].geometry.location.lat;
-        longitude = data.results[0].geometry.location.lng;
 
-        console.log("Latitude: " + latitude + "\nLongitude: " + longitude);
+            //assign the data to the appropriate variables
+            latitude = data.results[0].geometry.location.lat;
+            longitude = data.results[0].geometry.location.lng;
+            cityName = data.results[0].address_components[1].long_name;
+            stateName = data.results[0].address_components[3].short_name;
+        }catch (e){
+            console.error("Not a real zip code...please retry.");
+            process.exit();
+        };
+
+
+        //console.log("Latitude: " + latitude + "\nLongitude: " + longitude);
+
+        darkSky.get(longitude,latitude, cityName, stateName);
     });
 
-})
+});
+
+
+
+
